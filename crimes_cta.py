4e9e813b3e10c2eabd4c14a40a2c51db777fd0c8
Emily Zhang <zhange@cs.uchicago.edu>
2020-06-11 15:07:04 -0500

    updates to cta_crimes

--- crimes_cta.py
+++ crimes_cta.py
@@ -33,9 +33,11 @@ def gather_census(years, tables):
 
 
 def import_data():
+    """
+    Imports data for demographic, crime, and census geographies.
+    """
     years = [2013, 2014, 2015, 2016, 2017, 2018]
     tables = ['B02001_001E']
-    # import files
     crimes_df = pd.read_csv("data/Crimes-2013-2019.csv")
     crimes_gdf = gpd.GeoDataFrame(crimes_df, geometry=gpd.points_from_xy(crimes_df.Longitude, crimes_df.Latitude))
     census_gdf = gpd.read_file("https://data.cityofchicago.org/resource/bt9m-d2mf.geojson?$limit=9999999")
@@ -46,33 +48,47 @@ def import_data():
     return acs_df, crimes_gdf, census_gdf
 
 def merge_data(acs_df, crimes_gdf, census_gdf):
-    # merge data
+    """
+    Merges datasets into a single dataframe.
+    """
     merged_gdf = acs_df.merge(census_gdf, on="geo_12", how="inner")
     merged_gdf = gpd.GeoDataFrame(merged_gdf)
     allmerged_gdf = gpd.sjoin(crimes_gdf, merged_gdf, how='left')
-    limited_gdf = allmerged_gdf[["total_pop", "ID", "Case Number", "Year_left", "Primary Type", "blockce10", "GEO_ID", "geometry"]]
+    limited_gdf = allmerged_gdf[["total_pop", "ID", "Case Number", "Year_left", "Primary Type", "blockce10", "GEO_ID", "geometry", "Latitude", "Longitude"]]
     return limited_gdf
 
 def modify_data(df):
-    # clean data
+    """
+    Cleans dataframe
+    """
     df = df[df.total_pop != 0]
     df = df.dropna()
     df = df.drop_duplicates('Case Number')
     count_by_block = pd.Series(df.groupby(['GEO_ID', 'Year_left'])['Case Number'].count(), name='crime_count').reset_index()
-    count_by_block = count_by_block.rename(columns={"Year_left": "Year"})
-    joined2 = count_by_block.merge(acs_df, on=["GEO_ID","Year"], how="inner").reset_index()
+    joined2 = count_by_block.merge(df, on=["GEO_ID","Year_left"], how="inner").reset_index()
+    joined2 = joined2.rename(columns={"Year_left": "Year"})
     joined2 = joined2[joined2.total_pop != 0]
     joined2.dropna()
     joined2.reset_index()
     joined2['crimes_per_capita'] = joined2['crime_count']/joined2['total_pop']
-    # joined2['crimes_per_capita' > 1] impute with median
+    # impute crimes_per_capita with values > 1 with median
+    median = joined2.loc[joined2['crimes_per_capita']<1, 'crimes_per_capita'].median()
+    joined2.loc[joined2['crimes_per_capita'] > 1, 'crimes_per_capita'] = np.nan
+    joined2['crimes_per_capita'].fillna(median,inplace=True)
     final_gdf = gpd.GeoDataFrame(joined2)
     return final_gdf
 
 def visualize_crimes(df):
+    """
+    Plots crimes
+    """
     df.plot(figsize=(20, 20), column='crimes_per_capita', cmap=plt.cm.coolwarm, legend=True)
 
+
 def clean(df):
+    """
+    Converts formatting for location attribute
+    """
     split = df.split(',')
     lst = []
     for i in split:
@@ -81,38 +97,54 @@ def clean(df):
 
 
 def import_cta():
+    """
+    Imports CTA 'L' station data
+    """
     cta_df = pd.read_csv("data/CTA-LStops.csv")
     cta_df['location_split'] = cta_df['Location'].apply(lambda x: clean(x))
     cta_df['Longitude'] = cta_df['location_split'].map(lambda x: float(x[1]))
     cta_df['Latitude'] = cta_df['location_split'].map(lambda x: float(x[0]))
     cta_gdf = gpd.GeoDataFrame(cta_df, geometry=gpd.points_from_xy(cta_df.Longitude, cta_df.Latitude))
     cta_gdf = cta_gdf[['STOP_ID', 'STOP_NAME', 'STATION_NAME', 'Longitude', 'Latitude', 'geometry']]
-    census_gdf2 = import_merge_crimes() # this contains census merged with other info (since we want the point)
-    census_gdf2 = census_gdf2[['GEO_ID', 'Latitude', 'Longitude', 'geometry']]
-    return census_gdf2
+    return cta_gdf
 
     
 def near(point, pts):
-    # find the nearest point and return the corresponding Place value
+    """
+    Calculates nearest CTA station and returns the corresponding Stop ID.
+    """
     nearest = cta_gdf.geometry == nearest_points(point, pts)[1]
     return max(cta_gdf[nearest].STOP_ID)
 
 
-def nearest_point(df1, df2):
-    pts3 = df2.geometry.unary_union
-    df1['STOP_ID'] = df1.apply(lambda row: near(row.geometry, pts3), axis=1)
-    merged_with_cta = df1.merge(df1, on="STOP_ID", how="left")
+def nearest_point(merged_df2, cta_gdf):
+    """
+    Calculates nearest CTA station and returns updated dataset with nearest point.
+    """
+    pts3 = cta_gdf.geometry.unary_union
+    merged_df2['STOP_ID'] = merged_df2.apply(lambda row: near(row.geometry, pts3), axis=1)
+    merged_with_cta = merged_df2.merge(merged_df2, on="STOP_ID", how="left")
     return merged_with_cta
 
 def calc_distance(coords_1, coords_2):
+    """
+    Calculates distance between two coordinates
+    """
     return geopy.distance.distance(coords_1, coords_2).miles
 
+
 def gen_distance(df):
+    """
+    Generates distance between each point and its nearest CTA line
+    """
     df['distance'] = df.apply(lambda row: calc_distance((row.Longitude_x, row.Latitude_x), (row.Longitude_y, row.Latitude_y)), axis=1)
     final_cta = df[['GEO_ID','STOP_ID','STATION_NAME', 'distance']]
     return final_cta
 
 def visualize_cta(df):
+    """
+    Plots distances
+    """
     df = gpd.GeoDataFrame(df)
     df = df.rename(columns={"geometry_x": "geometry"})
     df.plot(figsize=(20, 20), column='distance', cmap=plt.cm.coolwarm, legend=True)
@@ -127,7 +159,7 @@ if __name__ == "__main__":
 
     # cta
     cta_gdf = import_cta()
-    cta_df2 = nearest_point(census_gdf2, cta_gdf) # need to separate these from merging
+    cta_df2 = nearest_point(merged_df2, cta_gdf) # need to separate these from merging
     cta_df3 = gen_distance(cta_df2)
     visualize_cta(cta_df3)
 
